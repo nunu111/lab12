6483783ae14f5eeb809b837be82e425fa4604135
nunu111 <nunu111za@hotmail.com>
2021-12-18 19:29:27 +0700

    all completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,52 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int i;
+
+int main()
+{
+    srand(time (0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    i = rand()%9;
+    switch (i)
+    {
+        case 0:
+            cout << "You will get A in this 261102.";
+            break;
+        case 1:
+            cout << "You will get B+ in this 261102.";
+            break;
+        case 2:
+            cout << "You will get B in this 261102.";
+            break;
+        case 3:
+            cout << "You will get C+ in this 261102.";
+            break;
+        case 4:
+            cout << "You will get C in this 261102.";
+            break;
+        case 5:
+            cout << "You will get D+ in this 261102.";
+            break;
+        case 6:
+            cout << "You will get D in this 261102.";
+            break;
+        case 7:
+            cout << "You will get F in this 261102.";
+            break;
+        case 8:
+            cout << "You will get W in this 261102.";
+            break;
+    
+    }
+}
